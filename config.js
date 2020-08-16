ed469f0f04d4d3811d7b03136ee2041ab3c4807a
eyadhany4 <69765899+eyadhany4@users.noreply.github.com>
2020-08-16 21:23:21 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN:"NzMzNzEwNDg4MTI2NDg4Njc3.XxHHJA.ncBCJJ8zpXevev2osWqS9W1k4VY", //تعديل مهم توكن بوتك
+  YT_API_KEY: "api ",
+  prefix: "=",
+  devs: ["708243857917345813"] ///تعديل مهم غير هذا الرقم بـ ايدي حسابك
+};
